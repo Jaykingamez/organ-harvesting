2e43027c474eb8d7e504495f95cae8309c8942cd
Jaykingamez <jaykinchan@gmail.com>
2022-08-20 16:26:22 +0800

    Update twineClass.ts

--- src/assets/app/class/twineClass.ts
+++ src/assets/app/class/twineClass.ts
@@ -1,14 +1,33 @@
+/*
+Stolen (mostly)/Inspired by https://gitgud.io/darkofocdarko/fort-of-chains implementation
+
+WIP: Implement types instead of using any...
+*/
+
 export default class TwineClass{
-    constructor(){
-
-    }
-
     clone() {
-        // this would refer to function, so need this.classReference
-        return new this(this);
+        return rebuildClassObject(this.constructor, this);
     }
 
     toJSON() {
-        return JSON.reviveWrapper(`new ${this.constructor.name}($ReviveData$)`, clone(this));
+        return toJsonHelper(this);
     }
+}
+
+function copyProperties(object: any, objectClass: any){
+    Object.keys(objectClass).forEach((attribute) => {
+        object[attribute] = clone(objectClass[attribute]);
+    });
+}
+
+function rebuildClassObject(classObject: any, argList: any){
+    var obj = Object.create(classObject.prototype);
+    copyProperties(obj, argList);
+    return obj;
+}
+
+function toJsonHelper(object: any){
+    var dataObject = {};
+    copyProperties(dataObject, object);
+    return JSON.reviveWrapper(`new ${object.constructor.name}($ReviveData$)`, dataObject);
 }
