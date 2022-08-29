ae353773d419e43bcce8eda0ac908377ef77547c
Jaykingamez <jaykinchan@gmail.com>
2022-08-29 16:03:24 +0800

    Add body into index.ts

--- src/assets/app/index.ts
+++ src/assets/app/index.ts
@@ -1,7 +1,10 @@
 import "./styles/index.scss";
 import {Player} from './class/character';
 import {Grave} from './class/grave';
+import {AliveBody, DeadBody} from './class/body';
 
 
 setup.Player = Player;
 setup.Grave = Grave;
+setup.AliveBody = AliveBody;
+setup.DeadBody = DeadBody;
