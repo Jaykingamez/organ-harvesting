7d04826f96792757f4f6d8c6ea363715378c2445
Jaykingamez <jaykinchan@gmail.com>
2022-09-06 11:33:43 +0800

    Add Npc class in index.ts

--- src/assets/app/index.ts
+++ src/assets/app/index.ts
@@ -1,10 +1,11 @@
 import "./styles/index.scss";
-import {Player} from './class/character';
+import {Player, Npc} from './class/character';
 import {Grave} from './class/grave';
 import {AliveBody, DeadBody} from './class/body';
 
 
 setup.Player = Player;
+setup.Npc = Npc;
 setup.Grave = Grave;
 setup.AliveBody = AliveBody;
 setup.DeadBody = DeadBody;
